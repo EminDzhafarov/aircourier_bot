7ae5dc4ae62506cde70ff386410a53b61666c54c
Emin <emin.djaf@gmail.com>
2023-05-13 12:02:50 +0300

    Убрал лишнее обращение к ДБ

--- filters/admin.py
+++ filters/admin.py
@@ -1,12 +1,7 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from db.models import Admins
+from settings import ADMINS
 
 class AdminFilter(BaseFilter):
-    async def __call__(self, message: Message, session: AsyncSession) -> bool:
-        query = await session.execute(select(Admins.user_id).filter_by(user_id=message.from_user.id))
-        status = query.scalar()
-        await session.commit()
-        return status == message.from_user.id
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in ADMINS
