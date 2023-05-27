aca89e7e29ce8ee06387202cb5dc878e4b15747f
Emin <emin.djaf@gmail.com>
2023-05-28 01:35:37 +0300

    Добавил рекламу, приветствие теперь только для новых юзеров

--- handlers/start_handlers.py
+++ handlers/start_handlers.py
@@ -15,62 +15,52 @@ from datetime import datetime
 
 router = Router()
 
-# Хэндлер на команду /start
-# @router.message(Command("start"), BlacklistFilter(), FlightsFilter())
-# async def cmd_start(message: Message, flights: bool, state: FSMContext, session: AsyncSession) -> None:
-#         stats_query = await session.execute(select(Stats).where(Stats.user_id == message.from_user.id))
-#         if not stats_query.scalar():
-#                 await session.merge(Stats(user_id=message.from_user.id, timestamp=datetime.now()))
-#         await session.commit()
-#         await state.set_state(StartStates.start)
-#         await message.answer("Привет! Этот бот поможет найти попутчиков для доставки посылок самолетом.\n\n"\
-#                 "<i>Отправляя сообщение, вы соглашаетесь на обработку персональных данных.</i>\n\n"\
-#                 "Для начала выберите"\
-#                              " что вы хотите сделать.", reply_markup=get_start_kb(flights))
-
 # Хэндлер на команду /start c deep link
 @router.message(Command("start"), BlacklistFilter(), FlightsFilter())
 async def cmd_start(
-        message: Message,
-        flights: bool,
-        state: FSMContext,
-        session: AsyncSession,
-        command: CommandObject
+    message: Message,
+    flights: bool,
+    state: FSMContext,
+    session: AsyncSession,
+    command: CommandObject
 ) -> None:
-        stats_query = await session.execute(select(Stats).where(Stats.user_id == message.from_user.id))
-        if not stats_query.scalar():
-                await session.merge(Stats(user_id=message.from_user.id, timestamp=datetime.now()))
-        await message.answer("Привет! Этот бот поможет найти попутчиков для доставки посылок самолетом.\n\n" \
-                             "<i>Отправляя сообщение, вы соглашаетесь на обработку персональных данных.</i>")
-        today = datetime.today()
-        if command.args:
-                try:
-                        query = (await session.scalars(select(Courier)
-                                                       .where(Courier.user_id == int(command.args))
-                                                       .where(Courier.flight_date >= today)
-                                                       .where(Courier.status == True)
-                                                       .order_by(Courier.flight_date))).all()
-                        if query:
-                                for courier in query:
-                                        await message.answer(f'<b>{courier.city_from} ✈ {courier.city_to}</b>\n'
-                                                             f'<b>Дата:</b> {courier.flight_date.strftime("%d.%m.%Y")}'
-                                                             f'\n<b>Имя:</b> <a href="tg://user?id={courier.user_id}">'
-                                                             f'{courier.user_name}</a>\n'
-                                                             f'<b>Контакт:</b> {courier.phone}\n'
-                                                             f'<b>Примечание:</b> {courier.info}',
-                                                             reply_markup=send_msg(courier.phone))
-                                await message.answer(
-                                    "Обратите внимание: некоторые курьеры запрещают писать себе настройками "
-                                    "приватности, но вы можете добавить их в контакты по номеру телефона "
-                                    "или позвонить.")
-                except ValueError:
-                        pass
-        await session.commit()
-        await message.answer("Выберите что вы хотите сделать.", reply_markup=get_start_kb(flights))
-        await state.set_state(StartStates.start)
+    stats_query = await session.execute(select(Stats).where(Stats.user_id == message.from_user.id))
+    if not stats_query.scalar():
+        await session.merge(Stats(user_id=message.from_user.id, timestamp=datetime.now()))
+        await message.answer("Привет!!! Этот бот поможет найти попутчиков для доставки посылок самолетом.\n\n" \
+                             "<i>Отправляя сообщение, вы соглашаетесь на обработку персональных данных.</i>\n\n"
+                             "Кстати, теперь у нас есть своя группа, где публикуются перелеты курьеров, "
+                             "подписывайтесь, чтобы ничего не пропустить: @aircourier_chat")
+
+    today = datetime.today()
+    if command.args:
+        try:
+            query = (await session.scalars(select(Courier)
+                                           .where(Courier.user_id == int(command.args))
+                                           .where(Courier.flight_date >= today)
+                                           .where(Courier.status == True)
+                                           .order_by(Courier.flight_date))).all()
+            if query:
+                for courier in query:
+                    await message.answer(f'<b>{courier.city_from} ✈ {courier.city_to}</b>\n'
+                                         f'<b>Дата:</b> {courier.flight_date.strftime("%d.%m.%Y")}'
+                                         f'\n<b>Имя:</b> <a href="tg://user?id={courier.user_id}">'
+                                         f'{courier.user_name}</a>\n'
+                                         f'<b>Контакт:</b> {courier.phone}\n'
+                                         f'<b>Примечание:</b> {courier.info}',
+                                         reply_markup=send_msg(courier.phone))
+                await message.answer(
+                    "Обратите внимание: некоторые курьеры запрещают писать себе настройками "
+                    "приватности, но вы можете добавить их в контакты по номеру телефона "
+                    "или позвонить.")
+        except ValueError:
+                pass
+    await session.commit()
+    await message.answer("Выберите что вы хотите сделать.", reply_markup=get_start_kb(flights))
+    await state.set_state(StartStates.start)
 
 # Хэндлер на текст "В начало"
 @router.message(Text(text="В начало", ignore_case=True), BlacklistFilter(), FlightsFilter())
 async def begining(message: Message, flights: bool, state: FSMContext) -> None:
-        await state.set_state(StartStates.start)
-        await message.answer("Выберите что вы хотите сделать.", reply_markup=get_start_kb(flights))
+    await state.set_state(StartStates.start)
+    await message.answer("Выберите что вы хотите сделать.", reply_markup=get_start_kb(flights))
