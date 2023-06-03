ad8c7319ef7e85ca362022324007a57d46179435
Emin <emin.djaf@gmail.com>
2023-06-03 12:41:41 +0300

    Разрешил использовать слэш в поле информации

--- bot/utils/misc/validators.py
+++ bot/utils/misc/validators.py
@@ -7,4 +7,4 @@ def isvalid_city(city):
     return re.search("^[a-zA-Zа-яА-ЯёЁ -]+$", city) and len(city) <= 20
 
 def isvalid_info(info):
-    return re.search("^[a-zA-Zа-яА-ЯёЁ0-9.,!?\n -]+$", info) and len(info) <= 200
+    return re.search("^[a-zA-Zа-яА-ЯёЁ0-9.,/!?\n -]+$", info) and len(info) <= 200
